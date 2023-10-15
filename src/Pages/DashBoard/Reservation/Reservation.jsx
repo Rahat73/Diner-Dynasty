5dd2a0b47db1a1cf7814c3d943312f8dc46b8262
Rahat73 <rahat.ashik.18@gmail.com>
2023-10-16 01:22:20 +0600

    reservation page and functionality added added

--- src/Pages/DashBoard/Reservation/Reservation.jsx
+++ src/Pages/DashBoard/Reservation/Reservation.jsx
@@ -3,8 +3,46 @@ import SectionHeader from "../../../Components/SectionHeader";
 import { useForm } from "react-hook-form";
 import Button from "../../../Components/Button";
 import { FaCalendarCheck } from "react-icons/fa";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../../Providers/AuthProvider";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { parseISO, format } from "date-fns";
+import { toast } from "react-toastify";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const Reservation = () => {
+  const { user } = useContext(AuthContext);
+  const [axiosSecure] = useAxiosSecure();
+  const [selectedDate, setSelcetedDate] = useState(""); //used to detect if date selected
+  const [selectedGuests, setSelectedGuests] = useState(""); //used to detect if guest selected
+
+  const { data: availableSlots = [], isLoading } = useQuery({
+    queryKey: ["selected-date", selectedDate, selectedGuests],
+    enabled: selectedDate !== "" && selectedGuests !== "",
+    queryFn: async () => {
+      const res = await axios(
+        `http://localhost:5000/booking-options?date=${selectedDate}&guests=${selectedGuests}`
+      );
+      return res.data;
+    },
+  });
+
+  // console.log(selectedDate, selectedGuests);
+  console.log(availableSlots);
+
+  const handleDateChange = (event) => {
+    //date selected, so make guest selectable and make the query as selectedDate changes values
+    setSelcetedDate(format(parseISO(event.target.value), "PP"));
+    console.log(format(parseISO(event.target.value), "PP"));
+  };
+
+  const handleGuestChange = (event) => {
+    //guest selected, so make timeSlot selectable and make the query as selectedGuest changes values
+    setSelectedGuests(event.target.value);
+    console.log(event.target.value);
+  };
+
   const {
     register,
     handleSubmit,
@@ -12,10 +50,37 @@ const Reservation = () => {
     reset,
   } = useForm();
 
+  const today = new Date();
+  const futureDate = new Date(today.getTime() + 5 * 24 * 60 * 60 * 1000);
+
   const onSubmit = (data) => {
-    reset();
-    console.log(data);
+    const { date, guests, timeSlot, phone } = data;
+
+    const bookingInfo = {
+      name: user?.displayName,
+      email: user?.email,
+      phone,
+      date: format(parseISO(date), "PP"),
+      guests,
+      timeSlot,
+    };
+    axiosSecure.post("/bookings", bookingInfo).then((res) => {
+      if (res.data.insertedId) {
+        toast.success(
+          <p>
+            Your table has been reserved. <br /> Please check{" "}
+            <span className="font-semibold">My Booking</span>
+          </p>
+        );
+        reset();
+        setSelcetedDate("");
+        setSelectedGuests("");
+      } else {
+        toast.error(<p>Somthing went wrong</p>);
+      }
+    });
   };
+
   return (
     <>
       <Helmet>
@@ -35,13 +100,27 @@ const Reservation = () => {
           <div className="form-control w-full md:col-span-6 lg:col-span-4">
             <label className="label">
               <span className="label-text">Date*</span>
+              {errors.date?.type === "min" && (
+                <p className="text-red-600 text-sm">
+                  {"Can only select future dates"}
+                </p>
+              )}
+              {errors.date?.type === "max" && (
+                <p className="text-red-600 text-sm">
+                  {"Can only select 5 days in advance"}
+                </p>
+              )}
             </label>
             <input
               type="date"
               placeholder="dd/mm/yy"
               className="input input-bordered w-full"
               required
-              {...register("date")}
+              {...register("date", {
+                min: new Date().toISOString(),
+                max: futureDate.toISOString(),
+                onBlur: handleDateChange,
+              })}
             />
           </div>
           <div className="form-control w-full md:col-span-6 lg:col-span-4">
@@ -51,13 +130,19 @@ const Reservation = () => {
             <select
               className="select select-bordered w-full"
               required
-              {...register("guest")}
+              defaultValue={""}
+              {...register("guests", {
+                onChange: handleGuestChange,
+              })}
             >
+              <option disabled value={""}>
+                Select an option
+              </option>
               <option>1</option>
               <option>2</option>
-              <option>3 or 4</option>
-              <option>5 or 6</option>
-              <option>7 ot 8</option>
+              <option>3-4</option>
+              <option>5-6</option>
+              <option>7-8</option>
             </select>
           </div>
           <div className="form-control w-full md:col-span-6 lg:col-span-4">
@@ -67,13 +152,20 @@ const Reservation = () => {
             <select
               className="select select-bordered w-full"
               required
+              defaultValue=""
               {...register("timeSlot")}
             >
-              <option>10:00</option>
-              <option>12:30</option>
-              <option>14:00</option>
-              <option>19:00</option>
-              <option>20:30</option>
+              <option disabled value={""}>
+                Select an option
+              </option>
+              {!isLoading &&
+                availableSlots.map((timeSlot) => (
+                  <option key={timeSlot.slot} value={timeSlot.slot}>
+                    {timeSlot.slot}{" "}
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    {timeSlot.left} of {timeSlot.capacity} left
+                  </option>
+                ))}
             </select>
           </div>
           <div className="form-control w-full md:col-span-6 lg:col-span-4">
@@ -83,9 +175,10 @@ const Reservation = () => {
             <input
               type="text"
               placeholder="e.g. Some One"
-              className="input input-bordered w-full"
+              className="input input-bordered w-full cursor-not-allowed"
               required
-              {...register("name")}
+              readOnly
+              defaultValue={user?.displayName}
             />
           </div>
           <div className="form-control w-full md:col-span-6 lg:col-span-4">
@@ -108,9 +201,10 @@ const Reservation = () => {
             <input
               type="email"
               placeholder="e.g. someone@mail.com"
-              className="input input-bordered w-full"
+              className="input input-bordered w-full cursor-not-allowed"
               required
-              {...register("email")}
+              readOnly
+              defaultValue={user?.email}
             />
           </div>
           <div className="col-span-12 flex justify-center">
